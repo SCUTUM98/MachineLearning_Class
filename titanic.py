0042eaddbb733c987ddac3c220659c08ce91bf0f
BONGEUNGU <rnqhsrms98@gmail.com>
2023-06-15 15:07:19 +0900

    TITANIC FUNCTION

--- titanic.py
+++ titanic.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.base import BaseEstimator
 import numpy as np
 
 # 입력된 나이에 따른 구분 값을 반환하는 함수
@@ -58,3 +59,40 @@ def exec_kfold(clf, xdf, ydf, folds = 5):
         print("교차 검증 정확도 [{}] {} ".format(iter_count, accuracy))
     mean_score = np.mean(scores)
     print("평균 정확도: ", mean_score)
+    
+def transform_features(df):
+    df['Age'].fillna(df['Age'].mean(), inplace=True)
+    df['Cabin'].fillna('N', inplace=True)
+    df['Embarked'].fillna('N', inplace=True)
+    df['Age_cate'] = df['Age'].apply(lambda x:get_category(x))
+    df = encode_features(df, ['Cabin', 'Sex', 'Embarked', 'Age_cate'])
+    df = df.drop(['PassengerId', 'Name', 'Ticket', 'Age'], axis=1)
+    
+    return df
+
+class MyDummyClassifier(BaseEstimator):
+    def fit(self, x, y = None):
+        pass
+    def predict(self, x):
+        pred = np.zeros((x.shape[0], 1))
+        
+        for i in range(x.shape[0]):
+            if x['Sex'].iloc[i] == 1:
+                pred[i] = 0
+            else:
+                pred[i] = 1
+        return pred
+    
+class MyFakeClassifier(BaseEstimator):
+    def fit(self, x, y = None):
+        pass
+    def predict(self, x):
+        return np.zeros((len(x), 1), dtype = bool)
+    
+def get_clf_eval(actual, pred):
+    confusion = confusion_matrix(actual, pred)
+    accuracy = accuracy_score(actual, pred)
+    precision = precision_score(actual, pred)
+    recall = recall_score(actual, pred)
+    
+    return accuracy, precision, recall
