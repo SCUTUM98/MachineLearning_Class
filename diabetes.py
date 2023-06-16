2b293f7ca31cc056fba74df982d7d1c523419719
BONGEUNGU <rnqhsrms98@gmail.com>
2023-06-16 12:34:14 +0900

    DIABETES FUNCTION

--- diabetes.py
+++ diabetes.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import Binarizer
 
 def get_model_eval(actual,pred):
     con = confusion_matrix(actual,pred)
@@ -9,3 +10,10 @@ def get_model_eval(actual,pred):
     print("오차 행렬\n",con)
     print(f"정확도:{acc:6.4f} 정밀도:{pre:6.4f}")
     print(f"재현율:{rec:6.4f} F1 점수:{f1:6.4f}")
+    
+def get_eval_by_thresholds(y_test,pred,thresholds):
+    for threshold in thresholds:
+        print("=== threshold:",threshold)
+        bn = Binarizer(threshold=threshold)
+        pv = bn.fit_transform(pred)
+        get_model_eval(y_test,pv)
