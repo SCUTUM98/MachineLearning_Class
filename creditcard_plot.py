d3c94bfc640c4ea53b4deea0e431350a92f0674e
BONGEUNGU <rnqhsrms98@gmail.com>
2023-06-21 12:21:27 +0900

    Kaggle creditcard plot functions

--- creditcard_plot.py
+++ creditcard_plot.py
@@ -29,6 +29,18 @@ def get_preprocessed_df(df=None):
     df_tmp.drop(['Time', 'Amount'], axis=1, inplace=True)
     
     return df_tmp
+def get_preprocessed_df2(df=None):
+    df_tmp = df.copy()
+    amount_n = np.log1p(df_tmp['Amount'])
+    df_tmp.insert(0, 'Amount_Scaled', amount_n)
+    df_tmp.drop(['Time', 'Amount'], axis=1, inplace=True)
+    
+    # 이상치 데이터 삭제
+    outlier_index = get_outlier(df=df_tmp, column='V14', weight=1.5)
+    df_tmp.drop(outlier_index, axis=0, inplace=True)
+    
+    return df_tmp
+    
 
 def get_outlier(df=None, column=None, weight=1.5):
     # fraud에 해당하는 column 데이터만 추출, 1/4 분위와 3/4 분위 지점을 np.percentile로 계산
